336420812607712a2709f4df03e28015ebf51dc8
aurelienAG <aurelien.gendrey@gmail.com>
2021-07-04 02:32:06 +0200

    Page d'accueil-fetching des produits
    affichage dynamique des produits sur la page d'accueil

---
+++ index.js
@@ -0,0 +1,30 @@
+//récupération des données de l'API
+function fetchData(){
+  fetch('http://localhost:3000/api/cameras')
+  .then(response=>{
+      if(!response.ok){
+          throw Error("ERROR");
+      }   
+  return response.json(); 
+})
+.then(data =>{
+      console.log(data);
+      const html = data.map(cameras => {
+          return `
+          <div class="cameras">
+          <img src="${cameras.imageUrl}" alt="${cameras.name}"/>
+          <h3>${cameras.name}</h3>
+          <p><strong>${cameras.price}</strong><a href="products.html"><span class="productPageLink">Voir le produit</a><span></p>
+          </div>
+          `;
+      })
+      .join("");
+      console.log(html);
+      document.querySelector('#allProducts').insertAdjacentHTML('afterbegin',html); 
+  })
+  .catch(error=>{
+      console.log(error);
+  });
+}
+
+ fetchData();
