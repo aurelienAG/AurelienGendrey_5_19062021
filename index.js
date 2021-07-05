ad90b57a43fdcd0b26b5751eb172957a896964a8
aurelienAG <aurelien.gendrey@gmail.com>
2021-07-05 17:28:50 +0200

    formatage des prix

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 //récupération des données de l'API
-function fetchData(){
-  fetch('http://localhost:3000/api/cameras')
-  .then(response=>{
-      if(!response.ok){
-          throw Error("ERROR");
-      }   
-  return response.json(); 
-})
-.then(data =>{
-      console.log(data);
-      const html = data.map(cameras => {
-          return `
+function fetchData() {
+    fetch('http://localhost:3000/api/cameras')
+        .then(response => {
+            if (!response.ok) {
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const html = data.map(cameras => {
+                return `
           <div class="cameras">
+          <a href="products.html">
           <img src="${cameras.imageUrl}" alt="${cameras.name}"/>
           <h3>${cameras.name}</h3>
-          <p><strong>${cameras.price}</strong><a href="products.html"><span class="productPageLink">Voir le produit</a><span></p>
+          <p><strong>${cameras.price / 100}€</strong></p>
+          </a>
           </div>
           `;
-      })
-      .join("");
-      console.log(html);
-      document.querySelector('#allProducts').insertAdjacentHTML('afterbegin',html); 
-  })
-  .catch(error=>{
-      console.log(error);
-  });
+            })
+                .join("");
+            console.log(html);
+            document.querySelector('#allProducts').insertAdjacentHTML('afterbegin', html);
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 
- fetchData();
+fetchData();
+
+//Selection du produit et renvoi sur la page produit
+const clickOnProduct = () => {
+
+}
