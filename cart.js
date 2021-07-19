e8922951fbccf70752ea4f302be77cae35033bf1
aurelienAG <aurelien.gendrey@gmail.com>
2021-07-19 16:23:40 +0200

    affichage des produits sur la page panier
    affichage des produits sur la page panier

---
+++ cart.js
@@ -0,0 +1,34 @@
+//Déclaration de la variable "savedInLocalStorage" 
+let savedInLocalStorage = JSON.parse(localStorage.getItem("productAdded"));
+//JSON.parse pour convertir les données JSON du local storage en objet javascript
+console.log(savedInLocalStorage);
+
+//----affichage des produits----
+
+const cartContained = document.querySelector("#orderResume");
+console.log(cartContained);
+
+//si aucun article dans le panier l'indiquer à l'utilisateur 
+if (savedInLocalStorage === null) {
+  const emptyCart = `
+  <h1 class="emptyPageText">Votre panier est vide</h1>
+  `;
+  cartContained.innerHTML = emptyCart;
+}
+//si le panier contient déjà un article
+else {
+  let productCartBloc = [];
+
+  for (k = 0; k < savedInLocalStorage.length; k++) {
+
+    productCartBloc = productCartBloc +
+      `<span id="productName"><h3>${savedInLocalStorage[k].name}</h3></span>
+        <span id="productLenses">${savedInLocalStorage[k].option}</span>
+        <span id="productQuantity"></span>
+        <span id="productPrice"><strong>${savedInLocalStorage[k].price}</strong></span>
+        `;
+  }
+  if (k == savedInLocalStorage.length) {
+    cartContained.innerHTML = productCartBloc;
+  }
+}
